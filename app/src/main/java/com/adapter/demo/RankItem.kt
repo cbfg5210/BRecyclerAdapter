4242f798f0bd2fc333e485a4221752acfeea77ae
沉璧浮光 <120276969@qq.com>
2019-09-19 15:43:02 +0800

    adapter:RankItem 重写 equals，hashCode 方法

--- app/src/main/java/com/adapter/demo/RankItem.kt
+++ app/src/main/java/com/adapter/demo/RankItem.kt
@@ -9,4 +9,21 @@ package com.adapter.demo
  * 修改时间：2018/7/12 9:52
  * 修改内容：
  */
-data class RankItem(var icon: String, var rank: Int)
+data class RankItem(var icon: String, var rank: Int) {
+    /**
+     * 重写以下方法,避免 List.contains 判断数据不准确
+     */
+    override fun equals(other: Any?): Boolean {
+        if (other is RankItem) {
+            return this.icon == other.icon && this.rank == other.rank
+        }
+        return super.equals(other)
+    }
+
+    /**
+     * 重写以下方法,避免 Set 存放相同值的对象
+     */
+    override fun hashCode(): Int {
+        return this.rank
+    }
+}
